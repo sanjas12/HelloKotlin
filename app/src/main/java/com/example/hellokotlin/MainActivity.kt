cf35ca13d48a6e697f0a685d935a3e54b857656b
alex_mint <sanja100v@gmail.com>
2020-05-29 11:34:36 +0300

    test

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.hellokotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+//test
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
