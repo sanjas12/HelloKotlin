f8a6e13b58e8e1093035fcfc19e2cc02d5bf794b
stovpyaga <sanja100v@gmail.com>
2020-06-02 16:53:13 +0300

    first app page. 11 book Learn Kotlin for Android Development.pdf

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.hellokotlin
+package kotlin.hello.hellokotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 //test
 
@@ -10,5 +14,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun go(v:View) {
+        text.setText("Hello Kotlin!" + "\n" + Date())
     }
 }
